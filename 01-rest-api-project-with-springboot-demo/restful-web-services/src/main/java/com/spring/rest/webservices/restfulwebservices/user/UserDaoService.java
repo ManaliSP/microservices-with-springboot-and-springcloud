64b20cd25c01cbdec80094d0629e5fa179592a4e
ManaliSP <manalip054@gmail.com>
2023-07-23 11:30:14 +0530

    code to retrieve all users

---
+++ 01-rest-api-project-with-springboot-demo/restful-web-services/src/main/java/com/spring/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,21 @@
+package com.spring.rest.webservices.restfulwebservices.user;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+    static {
+        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(1, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(1, "Jim", LocalDate.now().minusYears(20)));
+    }
+    public List<User> findAll(){
+        return users;
+    }
+}
---
+++ 01-rest-api-project-with-springboot-demo/restful-web-services/src/main/java/com/spring/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,23 @@
+package com.spring.rest.webservices.restfulwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService userDaoService;
+
+    @Autowired
+    public UserResource(UserDaoService userDaoService){
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return userDaoService.findAll();
+    }
+}
