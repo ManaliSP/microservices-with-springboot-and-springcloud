5afb6c188c74bad7fdce14ead2e57389308dffc4
ManaliSP <manalip054@gmail.com>
2023-07-23 13:22:45 +0530

    Added dependency for validation in pom.xml added validations for request

--- 01-rest-api-project-with-springboot-demo/restful-web-services/src/main/java/com/spring/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ 01-rest-api-project-with-springboot-demo/restful-web-services/src/main/java/com/spring/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,11 +1,15 @@
 package com.spring.rest.webservices.restfulwebservices.exception;
 
 import com.spring.rest.webservices.restfulwebservices.user.UserNotFountException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDate;
@@ -28,5 +32,13 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), "Total errors are: " + ex.getErrorCount() + " First error: " + ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
--- 01-rest-api-project-with-springboot-demo/restful-web-services/src/main/java/com/spring/rest/webservices/restfulwebservices/user/UserResource.java
+++ 01-rest-api-project-with-springboot-demo/restful-web-services/src/main/java/com/spring/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.spring.rest.webservices.restfulwebservices.user;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -33,7 +34,7 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
         User savedUser = userDaoService.save(user);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
