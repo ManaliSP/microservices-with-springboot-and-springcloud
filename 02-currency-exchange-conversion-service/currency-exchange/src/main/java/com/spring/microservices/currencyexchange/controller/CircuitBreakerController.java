ce95941302e02ac3dd8f70165ed5f5341b52bd3d
ManaliSP <manalip054@gmail.com>
2023-07-28 01:06:59 +0530

    Use of @CircuitBreaker

--- 02-currency-exchange-conversion-service/currency-exchange/src/main/java/com/spring/microservices/currencyexchange/controller/CircuitBreakerController.java
+++ 02-currency-exchange-conversion-service/currency-exchange/src/main/java/com/spring/microservices/currencyexchange/controller/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.spring.microservices.currencyexchange.controller;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +16,14 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
     public String sampleApi(){
 
         logger.info("sample api call received.");
         ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
-        return "Sample API.";
+        return responseEntity.getBody();
+//        return "Sample API.";
     }
 
     public String hardcodedResponse(Exception exception){
