ed3ee08234b1ee14b1664f4500e313476033d4da
ManaliSP <manalip054@gmail.com>
2023-07-28 00:48:13 +0530

    Configured resilience4j circuit breaker

--- 02-currency-exchange-conversion-service/currency-exchange/src/main/java/com/spring/microservices/currencyexchange/controller/CircuitBreakerController.java
+++ 02-currency-exchange-conversion-service/currency-exchange/src/main/java/com/spring/microservices/currencyexchange/controller/CircuitBreakerController.java
@@ -1,13 +1,28 @@
 package com.spring.microservices.currencyexchange.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
     @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
     public String sampleApi(){
+
+        logger.info("sample api call received.");
+        ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
         return "Sample API.";
+    }
+
+    public String hardcodedResponse(Exception exception){
+        return "fallback-response";
     }
 }
